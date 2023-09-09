86d32ddc8ace6e67307802fa2cd54a3f1b82bc9d
hellen-22 <hellenwain@gmail.com>
2023-09-09 10:27:11 +0300

    App set up

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'account',
+    'Mpesa',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'account.User'
